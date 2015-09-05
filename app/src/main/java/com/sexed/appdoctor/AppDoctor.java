030918a2135a973e0876d44aa56eedab19e79d0a
jgem12345 <Megaman12345>
2015-09-05 02:28:45 -0400

    Complete overhaul....

--- app/src/main/java/com/sexed/appdoctor/AppDoctor.java
+++ app/src/main/java/com/sexed/appdoctor/AppDoctor.java
@@ -2,151 +2,78 @@ package com.sexed.appdoctor;
 
 
 
-import com.sexed.appdoctor.R.drawable;
-
-import android.annotation.SuppressLint;
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 
-@SuppressLint("NewApi") public class AppDoctor extends Activity {
-    @SuppressLint("NewApi")
-	@Override
+
+public class AppDoctor extends Activity {
+
+    private Button education;
+    private Button utilities;
+    private Button about;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_doctor);
-        ImageButton sym=(ImageButton)findViewById(R.id.b_sym);
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR2) 
-		{
-			ActionBar actionbar=getActionBar();
-			actionbar.setDisplayHomeAsUpEnabled(true);
-			actionbar.setHomeAsUpIndicator(drawable.action_previous);
-		}
-		
-		
-        sym.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-			startSymptoms();	
-			}
-		});
-        ImageButton dis=(ImageButton)findViewById(R.id.b_dis);
-        dis.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				startDiseases();
-			}
-		});
-        ImageButton med=(ImageButton)findViewById(R.id.b_med);
-        med.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				startMedicines();
-			}
-		});
-        ImageButton news=(ImageButton)findViewById(R.id.b_health);
-        news.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				startNews();
-			}
-		});
-        ImageButton insp=(ImageButton)findViewById(R.id.b_insp);
-        insp.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				startInspiration();
-			}
-		});
-        ImageButton about=(ImageButton)findViewById(R.id.b_about);
+        education = (Button) findViewById(R.id.button);
+        utilities = (Button) findViewById(R.id.button2);
+        about = (Button) findViewById(R.id.button3);
+
+        education.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //start the education activity
+
+            }
+        });
+
+        utilities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //start the utilities menu activity
+            }
+        });
+
         about.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-			startAbout();	
-			}
-		});
-        
-    }
-    public void startSymptoms()
-    {
-	  Intent launchActivity=new Intent(this,Symptoms.class);
-	  startActivity(launchActivity);
-    }
-    public void startDiseases()
-    {
-	  Intent launchActivity=new Intent(this,Diseases.class);
-	  startActivity(launchActivity);
-    }
-    public void startMedicines()
-    {
-	  Intent launchActivity=new Intent(this,Medicines.class);
-	  startActivity(launchActivity);
-    }
-    public void startNews()
-    {
-	  startActivity(new Intent(Intent.ACTION_VIEW,
-	            Uri.parse("http://mobihealthnews.com/")));
-    }
-    public void startInspiration()
-    {
-	  Intent launchActivity=new Intent(this,InspirationalQuotes.class);
-	  startActivity(launchActivity);
-    }
-    public void startAbout()
-    {
-	  Intent launchActivity=new Intent(this,AboutMe.class);
-	  startActivity(launchActivity);
-    }
-    public void startDonate()
-    {
-      Intent launchActivity=new Intent(this,AccountNo.class);
-  	  startActivity(launchActivity);
+            @Override
+            public void onClick(View view) {
+                //start the about us activity
+            }
+        });
+
+
     }
 
-   
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-    	
-    	getMenuInflater().inflate(R.menu.donate, menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+//        getMenuInflater().inflate(R.menu.donate, menu);
         return true;
     }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	
-    	
-      switch (item.getItemId()) {
-      case R.menu.donate:
-      	  startDonate();
-      	  return true;  
-      
-      case android.R.id.home:
-            // app icon in action bar clicked; goto parent activity.
-            this.finish();
-            return true;
-                    
-        default:
-            return super.onOptionsItemSelected(item);
-      }
 
-   }
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()) {
+
+
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 }
